2315a0a26bd701299b6fe75ed3237ba99d910afe
Laxmikant Dikkar <laxmikantdikkar3753@gmail.com>
2025-12-02 15:26:17 +0530

    merging frontend

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar() {
       
         <nav className="navbar navbar-expand-lg ">
           <div className="container-fluid">
-            <Link to="/"><img src="/media/logo.svg" style={{ marginLeft: 300 }} width={150} alt="logo" /></Link>
+            <Link to="/"><img src="/media/logo.svg" style={{ marginLeft: 200 }} width={150} alt="logo" /></Link>
 
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
